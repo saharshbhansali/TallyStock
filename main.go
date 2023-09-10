9310e6696b589a610ca9da13ba0dd521744c6a61
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-09-10 17:13:31 +0530

    Update: basic GET request at /api made
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- main.go
+++ main.go
@@ -3,4 +3,25 @@ package main
 import (
 	"fmt"
 	"log"
+
+	"github.com/gofiber/fiber/v2"
 )
+
+func welcome(c *fiber.Ctx) error {
+	return c.SendString("Welcome to API")
+}
+
+
+func main() {
+	app := fiber.New()
+	app.Get("/api", welcome)
+
+	err := app.Listen(":3000")
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Println("Server is running on port 3000")
+	}
+
+}
+
