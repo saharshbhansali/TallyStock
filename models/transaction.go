6ccc59cba4d0331eacfaaa28c4d1db186a1aef5c
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-09-21 08:04:05 +0530

    Update: changed indexing on fields
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- models/transaction.go
+++ models/transaction.go
@@ -58,14 +58,14 @@ Notes:
 // check out gorm tags and features
 // check out validation and how to use
 type Transaction struct {
-	ID            uint      `json:"id" gorm:"primaryKey:pk_transaction_id" validate:"-"`
+	ID            uint      `json:"id" gorm:"primaryKey:pk_transaction_id;auto_increment" validate:"-"`
 	CreatedAt     time.Time `json:"created_at" validate:"-"`
 	UpdatedAt     time.Time `json:"updated_at" validate:"-"`
 	Date          string    `json:"date" validate:"date"`
 	InvoiceNumber string    `json:"invoice_number" gorm:"uniqueIndex:idx_invoice_hsn" validate:"required,min=3,max=50,alphanum"`
-	Destination   string    `json:"destination" validate:"required,min=2,max=50,alphanum"`            // gorm:"not null;check:party_name IN ('HO', 'Godown')"`
-	Status        string    `json:"status" gorm:"not null" validate:"required,oneof=In Out Transfer"` // gorm:"not null;check:status IN ('In', 'Out','Transfer')"`
-	HSNReferer       string    `json:"hsn_referer" validate:"required,min=3,max=50,alphanum"`               // gorm:"uniqueIndex:idx_invoice_hsn"
+	Destination   string    `json:"destination" validate:"required,min=2,max=50,alphanum"`                                    // gorm:"not null;check:party_name IN ('HO', 'Godown')"`
+	Status        string    `json:"status" gorm:"not null" validate:"required,oneof=In Out Transfer"`                         // gorm:"not null;check:status IN ('In', 'Out','Transfer')"`
+	HSNReferer    string    `json:"hsn_referer" gorm:"uniqueIndex:idx_invoice_hsn" validate:"required,min=3,max=50,alphanum"` // gorm:"uniqueIndex:idx_invoice_hsn"
 	Stock         Stock     `gorm:"foreignKey:hsn_referer;references:hsn_code" validate:"-"`
 	Supply        string    `json:"supply" gorm:"not null" validate:"required,min=2,max=50,alphanum"`
 	Quantity      float32   `json:"quantity" gorm:"default:0" validate:"gte=0"`
