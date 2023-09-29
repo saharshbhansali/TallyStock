4878fb725c43e336421f0dacf8bd0b8056b65063
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-09-29 06:51:34 +0530

    React-vite-ts

---
+++ frontend/react-vite-ts-app/src/pages/Employees/Employees.ts
@@ -0,0 +1,10 @@
+import React from "react";
+import EmployeeForm from "./EmployeeForm";
+
+export default function Employees() {
+    return (
+        <>
+        <EmployeeForm />
+        </>
+    );
+}
