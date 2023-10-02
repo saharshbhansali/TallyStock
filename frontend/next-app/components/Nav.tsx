493b7449bd4e13d984310b4765305d54f5ab8308
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-10-02 15:23:26 +0530

    Update: completed tutorial: https://www.youtube.com/watch\?v\=ABbww4CFQSo
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

---
+++ frontend/next-app/components/Nav.tsx
@@ -0,0 +1,34 @@
+"use client";
+import Image from "next/image";
+import { ModeToggle } from "./ui/theme-toggle";
+
+export default function Nav() {
+  return (
+    <header>
+      <nav>
+        <ul className="flex items-center justify-between">
+          <li>
+            <a
+              className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
+              href="/"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src="/vercel.svg"
+                alt="Vercel Logo"
+                className="dark:invert"
+                width={100}
+                height={24}
+                priority
+              />
+            </a>
+          </li>
+          <li>
+            <ModeToggle />
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
