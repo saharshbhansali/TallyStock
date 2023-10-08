94e6f8a4c64171b5eb2b28129a021aa5ad414402
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-10-08 07:42:20 +0530

    Refact: split transaction creation and updation functions
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- backend/Transactions.py
+++ backend/Transactions.py
@@ -8,9 +8,9 @@ def makeRequest(method, url, data=None):
     if method == 'GET':
         res = requests.get(url)
     elif method == 'POST':
-        res = requests.post(url, data)
+        res = requests.post(url, str(data))
     elif method == 'PUT':
-        res = requests.put(url, data)
+        res = requests.put(url, data=data)
     elif method == 'DELETE':
         res = requests.delete(url)
     
@@ -19,7 +19,8 @@ def makeRequest(method, url, data=None):
         res = json.loads(res.text)
         return res
     else:
-        print(res)
+        print(res.raw)
+        print(res.url, res.content)
         print(res.text)
         return None
 
@@ -64,72 +65,92 @@ def transactionPrintClient(T):
         quantity: {T['quantity']}
           ''')
 
+def CreateTransactionData(date, invoice_number, destination, status):
+    new_transactions = []
+    date = input("Enter the date of the transaction: ")
+    invoice_number = input("Enter the invoice number of the transaction: ")
+    destination = input("Enter the destination of the transaction: ")
+    status = input("Enter the status of the transaction: ")
+    entries = int(input("Enter the number of entries in this transaction to create: "))
+    for i in range(entries):
+        hsn_referer = input("Enter the hsn_referer of the transaction: ")
+        supply = input("Enter the supply of the transaction: ")
+        quantity = int(input("Enter the quantity of the transaction: "))
+        new_transaction = transactionJSON(date, invoice_number, destination, status, hsn_referer, supply, quantity)
+        new_transactions.append(new_transaction)
+    return new_transactions
+
+def CreateTransactionRequests(url, new_transaction=[]):
+        transactionPrintClient(new_transaction)
+        res = makeRequest('POST', url, json.dumps(new_transaction))
+        if res != None:
+            print("Transaction created successfully")
+        else:
+            print("Transaction not created")
+            return
+
+def UpdateTransactionData(old_transaction):
+    updated_transaction = transactionJSON(old_transaction['date'], old_transaction['invoice_number'], old_transaction['destination'], old_transaction['status'], old_transaction['hsn_referer'], old_transaction['supply'], old_transaction['quantity'])
+    flag = True
+    while flag:
+        item = int(input("Choose the fields to update:\n1. Date\n2. Invoice Number\n3. Destination\n4. Status\n5. HSN Referer\n6. Supply\n7. Quantity\n0. Exit and update\n"))
+        if item == 1:
+            date = input("Enter the new date: ")
+            updated_transaction['date'] = date
+        elif item == 2:
+            invoice_number = input("Enter the new invoice number: ")
+            updated_transaction['invoice_number'] = invoice_number
+        elif item == 3:
+            destination = input("Enter the new destination: ")
+            updated_transaction['destination'] = destination
+        elif item == 4:
+            status = input("Enter the new status: ")
+            updated_transaction['status'] = status
+        elif item == 5:
+            hsn_referer = input("Enter the new hsn_referer: ")
+            updated_transaction['hsn_referer'] = hsn_referer
+        elif item == 6:
+            supply = input("Enter the new supply: ")
+            updated_transaction['supply'] = supply
+        elif item == 7:
+            quantity = int(input("Enter the new quantity: "))
+            updated_transaction['quantity'] = quantity
+        elif item == 0:
+            flag = False
+        print("Modified:\n")
+        transactionPrintClient(updated_transaction)
+    return updated_transaction
+
+def UpdateTransactionRequests(url, updated_transaction, id):
+    confirm = input("Are you sure you want to update this transaction? (y/n): ")
+    if confirm == 'y':
+        # update the transaction
+        res = makeRequest('PUT', url+f'/{id}', json.dumps(updated_transaction))
+        if res != None:
+            print("Transaction updated successfully")
+        else:
+            print("Transaction not updated")
+            return
+        
 def transactionCRUD():
     choose_operation = int(input("Choose an operation to perform:\n1. Create\n2. Read\n3. Update\n4. Delete\n"))
+    ## Debugging
+    choose_operation = 1
     url = "http://localhost:3000/api/transactions"
 
     if choose_operation == 1:
         # Create a transaction
-        entries = int(input("Enter the number of entries in this transaction to create: "))
-        date = input("Enter the date of the transaction: ")
-        invoice_number = input("Enter the invoice number of the transaction: ")
-        destination = input("Enter the destination of the transaction: ")
-        status = input("Enter the status of the transaction: ")
-        for i in range(entries):
-            hsn_referer = input("Enter the hsn_referer of the transaction: ")
-            supply = input("Enter the supply of the transaction: ")
-            quantity = int(input("Enter the quantity of the transaction: "))
-            new_transaction = transactionJSON(date, invoice_number, destination, status, hsn_referer, supply, quantity)
-            # create the transaction
-            res = makeRequest('POST', url, json.dumps(new_transaction))
-            if res != None:
-                print("Transaction created successfully")
-            else:
-                print("Transaction not created")
-                return
+        new_transactions = CreateTransactionData()
+        for new_transaction in new_transactions:
+            CreateTransactionRequests(url, new_transaction)
+        # CreateTransactionRequests(url, [transactionJSON('2021-05-01', '123', 'Delhi', 'Pending', '123', 'Supply', 1000)])
 
     elif choose_operation == 3:
         id = int(input("Enter the id of the transaction to update: "))
-        flag = True
         old_transaction = makeRequest('GET', url+f'/{id}') # get the old transaction
         transactionPrintServer(old_transaction)
-        updated_transaction = transactionJSON(old_transaction['date'], old_transaction['invoice_number'], old_transaction['destination'], old_transaction['status'], old_transaction['hsn_referer'], old_transaction['supply'], old_transaction['quantity'])
-        while flag:
-            item = int(input("Choose the fields to update:\n1. Date\n2. Invoice Number\n3. Destination\n4. Status\n5. HSN Referer\n6. Supply\n7. Quantity\n0. Exit and update\n"))
-            if item == 1:
-                date = input("Enter the new date: ")
-                updated_transaction['date'] = date
-            elif item == 2:
-                invoice_number = input("Enter the new invoice number: ")
-                updated_transaction['invoice_number'] = invoice_number
-            elif item == 3:
-                destination = input("Enter the new destination: ")
-                updated_transaction['destination'] = destination
-            elif item == 4:
-                status = input("Enter the new status: ")
-                updated_transaction['status'] = status
-            elif item == 5:
-                hsn_referer = input("Enter the new hsn_referer: ")
-                updated_transaction['hsn_referer'] = hsn_referer
-            elif item == 6:
-                supply = input("Enter the new supply: ")
-                updated_transaction['supply'] = supply
-            elif item == 7:
-                quantity = int(input("Enter the new quantity: "))
-                updated_transaction['quantity'] = quantity
-            elif item == 0:
-                flag = False
-            print("Modified:\n")
-            transactionPrintClient(updated_transaction)
-        confirm = input("Are you sure you want to update this transaction? (y/n): ")
-        if confirm == 'y':
-            # update the transaction
-            res = makeRequest('PUT', url+f'/{id}', json.dumps(updated_transaction))
-            if res != None:
-                print("Transaction updated successfully")
-            else:
-                print("Transaction not updated")
-                return
+        updated_transaction = UpdateTransactionData(old_transaction)
+        UpdateTransactionRequests(url, updated_transaction, id)
         
     elif choose_operation == 4:
         id = int(input("Enter the id of the transaction to delete: "))
@@ -165,3 +186,5 @@ def transactionCRUD():
     
     print("Response from server:")
     transactionPrintServer(res)
+
+transactionCRUD()
