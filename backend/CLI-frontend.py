a4b2a4cd22a02a2eadcbf75853bf9cd7fc7f3f76
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-10-08 06:03:19 +0530

    Update: read working, fixing problems with CUD
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- backend/CLI-frontend.py
+++ backend/CLI-frontend.py
@@ -2,7 +2,7 @@
 import json
 import requests
 
-def transacion(date, invoice_number, destination, status, hsn_referer, stock, supply, quantity):
+def transactionJSON(date, invoice_number, destination, status, hsn_referer, supply, quantity):
     '''
     Return a JSON object for the transaction
     '''
@@ -17,7 +17,21 @@ def transacion(date, invoice_number, destination, status, hsn_referer, stock, su
     }
     return T
 
-def stock(hsn_code, stock_name, total_quantity, ho_quantity, godown_quantity):
+def transactionPrint(T):
+    print(f'''
+    Transaction:
+        id: {T['id']}
+        date: {T['date']}
+        invoice_number: {T['invoice_number']}
+        destination: {T['destination']}
+        status: {T['status']}
+        hsn_referer: {T['hsn_referer']}
+        supply: {T['supply']}
+        quantity: {T['quantity']}
+        updated_at: {T['updated_at']}
+          ''')
+
+def stockJSON(hsn_code, stock_name, total_quantity, ho_quantity, godown_quantity):
     '''
     Return a JSON object for the stock
     '''
@@ -29,6 +43,18 @@ def stock(hsn_code, stock_name, total_quantity, ho_quantity, godown_quantity):
         'godown_quantity': godown_quantity,
     }
     return S
+
+def stockPrint(S):
+    print(f'''
+    Stock:
+        id: {S['id']}
+        hsn_code: {S['hsn_code']}
+        stock_name: {S['stock_name']}
+        total_quantity: {S['total_quantity']}
+        ho_quantity: {S['ho_quantity']}
+        godown_quantity: {S['godown_quantity']}
+        updated_at: {S['updated_at']}
+          ''')
 
 def makeRequest(method, url, data=None):
     '''
@@ -42,12 +68,19 @@ def makeRequest(method, url, data=None):
         res = requests.put(url, data)
     elif method == 'DELETE':
         res = requests.delete(url)
-    return res
+    
+    print("Response from server:",res.status_code, res.reason)
+    if res.status_code == 200:
+        res = json.loads(res.text)
+        return res
+    else:
+        print(res)
+        return None
 
 def transactionCRUD():
     choose_operation = int(input("Choose an operation to perform:\n1. Create\n2. Read\n3. Update\n4. Delete\n"))
     url = "http://localhost:3000/api/transactions"
-    res = 'Error'
+
     if choose_operation == 1:
         # Create a transaction
         date = input("Enter the date of the transaction: ")
@@ -55,80 +88,99 @@ def transactionCRUD():
         destination = input("Enter the destination of the transaction: ")
         status = input("Enter the status of the transaction: ")
         hsn_referer = input("Enter the hsn_referer of the transaction: ")
-        stock = input("Enter the stock of the transaction: ")
         supply = input("Enter the supply of the transaction: ")
         quantity = int(input("Enter the quantity of the transaction: "))
-        T = transacion(date, invoice_number, destination, status, hsn_referer, supply, quantity)
+        new_transaction = transactionJSON(date, invoice_number, destination, status, hsn_referer, supply, quantity)
         # create the transaction
-        res = makeRequest('POST', url, T)
+        res = makeRequest('POST', url, json.dumps(new_transaction))
+        if res != None:
+            print("Transaction created successfully")
+        else:
+            print("Transaction not created")
+            return
 
     elif choose_operation == 3:
         id = int(input("Enter the id of the transaction to update: "))
         flag = True
         old_transaction = makeRequest('GET', url+f'/{id}') # get the old transaction
-        print("Old transaction:\n", old_transaction)
-        T = eval(old_transaction)
+        transactionPrint(old_transaction)
+        updated_transaction = old_transaction
         while flag:
             item = int(input("Choose the fields to update:\n1. Date\n2. Invoice Number\n3. Destination\n4. Status\n5. HSN Referer\n6. Supply\n7. Quantity\n0. Exit and update\n"))
             if item == 1:
                 date = input("Enter the new date: ")
-                T['date'] = date
+                updated_transaction['date'] = date
             elif item == 2:
                 invoice_number = input("Enter the new invoice number: ")
-                T['invoice_number'] = invoice_number
+                updated_transaction['invoice_number'] = invoice_number
             elif item == 3:
                 destination = input("Enter the new destination: ")
-                T['destination'] = destination
+                updated_transaction['destination'] = destination
             elif item == 4:
                 status = input("Enter the new status: ")
-                T['status'] = status
+                updated_transaction['status'] = status
             elif item == 5:
                 hsn_referer = input("Enter the new hsn_referer: ")
-                T['hsn_referer'] = hsn_referer
+                updated_transaction['hsn_referer'] = hsn_referer
             elif item == 6:
                 supply = input("Enter the new supply: ")
-                T['supply'] = supply
+                updated_transaction['supply'] = supply
             elif item == 7:
                 quantity = int(input("Enter the new quantity: "))
-                T['quantity'] = quantity
+                updated_transaction['quantity'] = quantity
             elif item == 0:
                 flag = False
-            print("Modified transaction:\n", T)
+            print("Modified:\n")
+            transactionPrint(updated_transaction)
         confirm = input("Are you sure you want to update this transaction? (y/n): ")
         if confirm == 'y':
             # update the transaction
-            res = makeRequest('PUT', url+f'/{id}', T)
+            res = makeRequest('PUT', url+f'/{id}', json.dumps(updated_transaction))
+            if res != None:
+                print("Transaction updated successfully")
+            else:
+                print("Transaction not updated")
+                return
         
     elif choose_operation == 4:
         id = int(input("Enter the id of the transaction to delete: "))
-        transacion = requests.get() # get the transaction
-        print("Transaction to be deleted:\n", transaction)
+        transaction = makeRequest('GET', url+f'/{id}') # get the transaction
+        print("Deleting:\n")
+        transactionPrint(transaction)
         delete = input("Are you sure you want to delete this transaction? (y/n): ")
         if delete == 'y':
             # delete the transaction
             res = makeRequest('DELETE', url+f'/{id}')
+            if res != None:
+                print("Transaction deleted successfully")
+            else:
+                print("Transaction not deleted")
+                return
         else:
             print("Transaction not deleted")
 
     elif choose_operation == 2:
-        id = int(input("Enter the id of the transaction to read, 0 for all transactions: "))
-        if id == 0:
+        id = input("Enter the id of the transaction to read (leave empty for all transactions): ")
+        if id == '':
             # Read all transactions
             transactions = makeRequest('GET', url)
             for transaction in transactions:
-                print("Transaction:\n", transaction)
-            res = 'All transactions read successfully'
+                transactionPrint(transaction)
+            print("Response from server:")
+            print('All transactions read successfully')
+            return
         else:
+            id = int(id)
             # Read a transaction
             res = makeRequest('GET', url+f'/{id}')
     
-    print("Response from server:\n")
-    print(res)
+    print("Response from server:")
+    transactionPrint(res)
 
 def stockCRUD():
     choose_operation = int(input("Choose an operation to perform:\n1. Create\n2. Read\n3. Update\n4. Delete\n"))
     url = "http://localhost:3000/api/stocks"
-    res = 'Error'
+
     if choose_operation == 1:
         # Create a stock
         hsn_code = input("Enter the hsn_code of the stock: ")
@@ -136,75 +188,100 @@ def stockCRUD():
         total_quantity = int(input("Enter the total_quantity of the stock: "))
         ho_quantity = int(input("Enter the ho_quantity of the stock: "))
         godown_quantity = int(input("Enter the godown_quantity of the stock: "))
-        S = stock(hsn_code, stock_name, total_quantity, ho_quantity, godown_quantity)
+        new_stock = stockJSON(hsn_code, stock_name, total_quantity, ho_quantity, godown_quantity)
         # create the stock
-        res = makeRequest('POST', url, S)
+        res = makeRequest('POST', url, json.dumps(new_stock))
         print("Stock created successfully", res)
+        if res != None:
+                print("Stock updated successfully")
+        else:
+            print("Stock not updated")
+            return
 
     elif choose_operation == 3:
         id = int(input("Enter the id of the stock to update: "))
         flag = True
         old_stock = makeRequest('GET', url+f'/{id}') # get the old stock
-        print("Old stock:\n", old_stock)
-        S = eval(old_stock)
+        stockPrint(old_stock)
+        updated_stock = old_stock
         while flag:
             item = int(input("Choose the fields to update:\n1. HSN Code\n2. Stock Name\n3. Total Quantity\n4. HO Quantity\n5. Godown Quantity\n0. Exit and update\n"))
             if item == 1:
                 hsn_code = input("Enter the new hsn_code: ")
-                S['hsn_code'] = hsn_code
+                updated_stock['hsn_code'] = hsn_code
             elif item == 2:
                 stock_name = input("Enter the new stock_name: ")
-                S['stock_name'] = stock_name
+                updated_stock['stock_name'] = stock_name
             elif item == 3:
                 total_quantity = int(input("Enter the new total_quantity: "))
-                S['total_quantity'] = total_quantity
+                updated_stock['total_quantity'] = total_quantity
             elif item == 4:
                 ho_quantity = int(input("Enter the new ho_quantity: "))
-                S['ho_quantity'] = ho_quantity
+                updated_stock['ho_quantity'] = ho_quantity
             elif item == 5:
                 godown_quantity = int(input("Enter the new godown_quantity: "))
-                S['godown_quantity'] = godown_quantity
+                updated_stock['godown_quantity'] = godown_quantity
             elif item == 0:
                 flag = False
-            print("Modified stock:\n", S)
+            print("Modified:\n")
+            stockPrint(updated_stock)
         confirm = input("Are you sure you want to update this stock? (y/n): ")
         if confirm == 'y':
             # update the stock
-            res = makeRequest('PUT', url+f'/{id}', S)
+            res = makeRequest('PUT', url+f'/{id}', json.dumps(updated_stock))
+            if res != None:
+                print("Stock updated successfully")
+            else:
+                print("Stock not updated")
+                return
         
     elif choose_operation == 4:
         id = int(input("Enter the id of the stock to delete: "))
         stock = makeRequest('GET', url+f'/{id}') # get the stock
-        print("Stock to be deleted:\n", stock)
+        print("Deleting:")
+        stockPrint(stock)
         delete = input("Are you sure you want to delete this stock? (y/n): ")
         if delete == 'y':
             # delete the stock
             res = makeRequest('DELETE', url+f'/{id}')
+            if res != None:
+                print("Stock deleted successfully")
+            else:
+                print("Stock not deleted")
+                return
         else:
             print("Stock not deleted")
 
     elif choose_operation == 2:
-        id = int(input("Enter the id of the stock to read, 0 for all stocks: "))
-        if id == 0:
+        id = input("Enter the id of the stock to read (leave empty for all stocks): ")
+        if id == '':
             # Read all stocks
-            stocks = requests.get()
+            stocks = makeRequest('GET', url)
             for stock in stocks:
-                print("Stock:\n", stock)
-            res = 'All stocks read successfully'
+                stockPrint(stock)
+            print("Response from server:")
+            print('All stocks read successfully')
+            return
         else:
+            id = int(id)
             # Read a stock
             res = makeRequest('GET', url+f'/{id}')
     
-    print("Response from server:\n")
-    print(res)
+    print("Response from server:")
+    stockPrint(res)
 
 def main():
     '''
     Main function
     '''
+    while True:
+        choose_item = input("Choose an item to perform CRUD operations on:\n1. Transaction\n2. Stock\n")
+        if choose_item == '1':
+            transactionCRUD()
+        elif choose_item == '2':
+            stockCRUD()
+        else:
+            print("Invalid choice")
+            break
 
-    choose_item = int(input("Choose an item to perform CRUD operations on:\n1. Transaction\n2. Stock\n"))
-    if choose_item == 1:
-        transactionCRUD()
-    elif choose_item == 2:
-        stockCRUD()
+main()
