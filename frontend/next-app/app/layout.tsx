cc9b051cfe0778929f09c118a5b1ff40e579c68e
saharshbhansali <saharsh.bhansali15@gmail.com>
2023-10-11 02:49:51 +0530

    Update: create fixed layout for all pages in layout.tsx
    Signed-off-by: saharshbhansali <saharsh.bhansali15@gmail.com>

--- frontend/next-app/app/layout.tsx
+++ frontend/next-app/app/layout.tsx
@@ -1,6 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { ThemeProvider } from "@/components/ui/theme-provider";
+import { ThemeProvider } from "@ui/theme-provider";
+import Nav from "@comp/Nav";
+import { Button } from "@ui/button";
+import { ProfileForm } from "@comp/UserForm";
 
 export const metadata: Metadata = {
   title: "TallyStock",
@@ -15,9 +18,23 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>
-        <main className="max-w-6xl mx-auto">
+        <main className="max-w-6xl mx-auto p-24">
           <ThemeProvider attribute="class" defaultTheme="system">
-            {children}
+            <Nav />
+            <section className="py-12 flex flex-col items-center text-center gap-8">
+              <h1 className="text-4xl font-bold">Shadcn is awesome</h1>
+              <p className="text-2xl text-foreground">
+                Hand picked themes to copy and paste
+              </p>
+            </section>
+            <div className="flex gap-3 items-center justify-center ">
+              <Button variant={"secondary"}>Learn More</Button>
+              <Button variant={"secondary"}>Enroll Now</Button>
+            </div>
+            <div>{children}</div>
+            {/* <div className="gap-4 py-6 flex flex-col items-center">
+                <ProfileForm />
+              </div> */}
           </ThemeProvider>
         </main>
       </body>
--- frontend/next-app/app/page.tsx
+++ frontend/next-app/app/page.tsx
@@ -1,26 +1,23 @@
-import Nav from "@components/Nav";
-import { Button } from "@components/ui/button";
-import { ProfileForm } from "@components/UserForm";
 import Image from "next/image";
+import { useEffect, useState } from "react";
 
 export default function Home() {
-  return (
-    <main className=" p-24">
-      <Nav />
-      <section className="py-12 flex flex-col items-center text-center gap-8">
-        <h1 className="text-4xl font-bold">Shadcn is awesome</h1>
-        <p className="text-2xl text-foreground">
-          Hand picked themes to copy and paste
-        </p>
-      </section>
-      <div className="flex gap-3 items-center justify-center ">
-        <Button variant={"secondary"}>Learn More</Button>
-        <Button variant={"secondary"}>Enroll Now</Button>
-      </div>
+  const [stocks, setStocks] = useState([]);
 
-      <div className="gap-4 py-6 flex flex-col items-center">
-        <ProfileForm />
-      </div>
-    </main>
+  useEffect(() => {
+    async function fetchStocks() {
+      const response = await fetch("http://localhost:3000/api/transactions");
+      const data = await response.json();
+      console.log(data);
+      setStocks(data);
+    }
+
+    fetchStocks();
+  }, []);
+
+  return (
+    <>
+      <div>{stocks}</div>
+    </>
   );
 }
